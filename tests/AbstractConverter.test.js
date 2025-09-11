d6e813a3d8d13f8925505c1e0305fc25f9e69b0d
Andreas Eriksson <ae225ak@student.lnu.se>
2025-09-11 07:47:34 +0200

    Install Vitest test framework with first test

---
+++ tests/AbstractConverter.test.js
@@ -0,0 +1,8 @@
+import { describe, it, expect } from 'vitest'
+import { AbstractConverter } from '../src/converters/AbstractConverter.js'
+
+describe('AbstractConverter', () => {
+    it('should throw error when instantiated', () => {
+        expect(() => new AbstractConverter()).toThrow()
+    })
+})
