d5d66a46267cdb23640faf227a5a9bfbd263388a
Andreas Eriksson <ae225ak@student.lnu.se>
2025-09-11 07:37:41 +0200

    Update HeadingConverter to extend AbstractConv

--- src/converters/HeadingConverter.js
+++ src/converters/HeadingConverter.js
@@ -1,3 +1,9 @@
 import { AbstractConverter } from "./AbstractConverter";
 
-export class HeadingConverter extends AbstractConverter {}
+export class HeadingConverter extends AbstractConverter {
+
+    convert(input) {
+
+        
+    }
+}
