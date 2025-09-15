bd9802c248b13ae16de09fa9fec704c4845eca2e
Andreas Eriksson <ae225ak@student.lnu.se>
2025-09-15 08:35:56 +0200

    Add first h-conversion that passes test

--- src/converters/HeadingConverter.js
+++ src/converters/HeadingConverter.js
@@ -3,7 +3,37 @@ import { AbstractConverter } from "./AbstractConverter";
 export class HeadingConverter extends AbstractConverter {
 
     convert(input) {
-        return '<h1>Hello 1DV610!</h1>'
-        
+        // return '<h1>Hello 1DV610!</h1>'
+
+        const lines = input.split('\n') // split into lines
+
+        const converted = lines.map(line => { // map the lines and run conversion
+            if (line.startsWith('#')) {
+                return this.#convertHeading(line)
+            }
+
+            return line
+        })
+
+        return converted.join('\n') // Reassemble to string
+
+    }
+
+    #convertHeading(line) {
+        let numberOfHashChars = 0
+
+
+        for (let i = 0; i < line.length; i++) { // Count number of hashes (heading level 1-6)
+            if (line[i] === '#') {
+                numberOfHashChars++
+            }
+        }
+
+        const headingTextContent = line.substring(numberOfHashChars + 1) // Get the text after the hashes
+
+        return `<h${numberOfHashChars}>${headingTextContent}</h${numberOfHashChars}>`
+
+
+
     }
 }
