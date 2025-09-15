8dfc6b5978a13b23d371092dccc3bc6586300e88
Andreas Eriksson <ae225ak@student.lnu.se>
2025-09-15 14:46:07 +0200

    Work on InlineStyleConverter

--- src/converters/InlineStyleConverter.js
+++ src/converters/InlineStyleConverter.js
@@ -11,9 +11,9 @@ export class InlineStyleConverter extends AbstractConverter {
             line = this.#convertItalic(line)
         }
 
-        if (line.includes('~~')) {
-            line = this.#convertStrikethrough(line)
-        }
+        // if (line.includes('~~')) {
+        //     line = this.#convertStrikethrough(line)
+        // }
 
         return line
     }
@@ -21,6 +21,13 @@ export class InlineStyleConverter extends AbstractConverter {
 
     #convertBold(line) {
         // Find index of first and second **
+        const firstOccurance = line.indexOf('**')
+        const secondOccurrence = line.indexOf('**', firstOccurance + 2)
+
+        const textToBold = line.substring(firstOccurance + 2, secondOccurrence)
+
+        return `<strong>${textToBold}</strong>`
+
         // convert substring with textcontent to html, ** textcontent ** ---> <strong>textcontent</strong>
         // Search again until no more **.
         // How to handle that lists uses *?
