5bf7c6ce3bd28fafe08119a4ec13ca07da128642
Andreas Eriksson <ae225ak@student.lnu.se>
2025-09-15 09:17:43 +0200

    Fix whitespace error in multiline test

--- tests/MarkupConverter.test.js
+++ tests/MarkupConverter.test.js
@@ -5,12 +5,25 @@ describe('MarkupConverter', () => {
     it('should convert markdown heading to html heading', () => {
         const converter = new MarkupConverter()
         const result = converter.convert('# Hello 1DV610!')
-        expect(result).toContain('<h1>Hello 1DV610!</h1>')
+        expect(result).toBe('<h1>Hello 1DV610!</h1>')
     })
 
     it('should convert the headings in a multi-line markdown text', () => {
         const converter = new MarkupConverter()
-        const result = converter.convert('# Hello 1DV610!\n## This is a MarkupConverter.\n### It converts between Markup formats!\n#### I chose this module to get some string manipulation practice.\n##### Will it be useful?\n###### Who knows!')
-        expect(result).toContain('<h1>Hello 1DV610!</h1>\n<h2>This is a MarkupConverter.</h2>\n<h3>It converts between Markup formats!</h3>\n<h4>I chose this module to get some string manipulation practice.</h4>\n<h5>Will it be useful?</h5>\n<h6>Who knows!</h6>')
+        const input = `# Hello 1DV610!
+## This is a MarkupConverter.
+### It converts between Markup formats!
+#### I chose this module to get some string manipulation practice.
+##### Will it be useful?
+###### Who knows!`
+        const expected = `<h1>Hello 1DV610!</h1>
+<h2>This is a MarkupConverter.</h2>
+<h3>It converts between Markup formats!</h3>
+<h4>I chose this module to get some string manipulation practice.</h4>
+<h5>Will it be useful?</h5>
+<h6>Who knows!</h6>`
+
+        const result = converter.convert(input)
+        expect(result).toBe(expected)
     })
 })
