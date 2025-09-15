01ce7e3fdf4bcb342ddff55f5fa4e7e466a9e43b
Andreas Eriksson <ae225ak@student.lnu.se>
2025-09-15 09:04:36 +0200

    Add test for text containting multiple headings

--- tests/MarkupConverter.test.js
+++ tests/MarkupConverter.test.js
@@ -6,5 +6,11 @@ describe('MarkupConverter', () => {
         const converter = new MarkupConverter()
         const result = converter.convert('# Hello 1DV610!')
         expect(result).toContain('<h1>Hello 1DV610!</h1>')
-    } )
+    })
+
+    it('should convert the headings in a multi-line markdown text', () => {
+        const converter = new MarkupConverter()
+        const result = converter.convert('# Hello 1DV610!\n## This is a MarkupConverter.\n### It converts between Markup formats!\n#### I chose this module to get some string manipulation practice.\n##### Will it be useful?\n###### Who knows!')
+        expect(result).toContain('<h1>Hello 1DV610!</h1>\n<h2>This is a MarkupConverter.</h2>\n<h3>It converts between Markup formats!</h3>\n<h4>I chose this module to get some string manipulation practice.</h4>\n<h5>Will it be useful?</h5>\n<h6>Who knows!</h6>')
+    })
 })
