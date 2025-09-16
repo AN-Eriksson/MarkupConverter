99d2f1e2e1692355353a504adaee49316df17b06
Andreas Eriksson <ae225ak@student.lnu.se>
2025-09-16 08:19:12 +0200

    Add logic to run converters in sequence

--- src/core/MarkupConverter.js
+++ src/core/MarkupConverter.js
@@ -1,16 +1,24 @@
 import { HeadingConverter } from "../converters/HeadingConverter"
+import { InlineStyleConverter } from "../converters/InlineStyleConverter"
 
 export class MarkupConverter {
     #converters
 
     constructor() {
         this.#converters = new Map([
-            ['heading', new HeadingConverter()]
+            ['heading', new HeadingConverter()],
+            ['inlineStyle', new InlineStyleConverter()]
         ])
     }
 
 
     convert(text) {
-        return this.#converters.get('heading').convert(text)
+        let result = text
+
+        for (const [name, converter] of this.#converters) {
+            result = converter.convert(result)
+        }
+
+        return result
     }
 }
