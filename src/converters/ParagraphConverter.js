72ee323dcae8a76a4d0662990db80e63eeb56fbb
Andreas Eriksson <ae225ak@student.lnu.se>
2025-09-18 21:14:35 +0200

    Add JSDoc explaining the overridden convert()

--- src/converters/ParagraphConverter.js
+++ src/converters/ParagraphConverter.js
@@ -1,6 +1,15 @@
 import { AbstractConverter } from "./AbstractConverter";
 
 export class ParagraphConverter extends AbstractConverter {
+    /**
+     * Converts text blocks into HTML paragraphs while preserving headings
+     * Overrides AbstractConverter.convert() to handle block-level paragraph processing
+     * 
+     * @param {string} text - The input text to convert
+     * @returns {string} Text with non-heading blocks wrapped in <p> tags
+     * 
+     * @override
+     */
     convert(text) {
         const textBlocks = text.split('\n\n')
 
