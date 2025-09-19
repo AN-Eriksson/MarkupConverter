69fa2f66a084265fb6d45ce8baa2a8c4eff395e1
Andreas Eriksson <ae225ak@student.lnu.se>
2025-09-19 12:23:29 +0200

    Pass first test for unordered lists

--- src/converters/ListConverter.js
+++ src/converters/ListConverter.js
@@ -4,19 +4,16 @@ export class ListConverter extends AbstractConverter {
 
     convert(text) {
         const textBlocks = text.split('\n\n')
-
-        console.log('Text blocks:')
-        console.log(textBlocks)
-
         const taggedBlocks = textBlocks.map(block => {
 
             if (this.#isUnorderedList(block)) {
+                console.log(block)
                 return this.#convertUnorderedList(block)
             }
 
-            if (this.#isOrderedList(block)) {
-                return this.#convertOrderedList(block)
-            }
+            // if (this.#isOrderedList(block)) {
+            //     return this.#convertOrderedList(block)
+            // }
 
             return block
         })
@@ -24,14 +21,31 @@ export class ListConverter extends AbstractConverter {
         return taggedBlocks.join('\n\n')
     }
 
+    #isUnorderedList(textBlock) {
+        const firstLine = textBlock.split('\n')[0]
+        return firstLine.startsWith('- ')
+    }
+
     #isOrderedList(textBlock) {
         const firstLine = textBlock.split(('\n'))[0]
         return firstLine.trim().match(/^\d+\.\s/)
     }
 
-    #isUnorderedList(textBlock) {
-        const firstLine = textBlock.split('\n')[0]
-        return firstLine.startsWith('- ')
+    #convertUnorderedList(block) {
+        const lines = block.split('\n')
 
+        const listItems = lines.map(line => {
+            const itemTextContent = line.substring(2)
+            return `<li>${itemTextContent}</li>`
+        })
+
+        const joinedList = listItems.join('\n')
+
+        console.log(joinedList)
+        console.log(`<ul>\n${joinedList}\n</ul>`)
+
+        return `<ul>\n${joinedList}\n</ul>`
     }
+
+
 }
