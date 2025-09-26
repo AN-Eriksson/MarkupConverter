1c339b84b9c875b9d6a345912dfca8c4e96e1799
Andreas Eriksson <ae225ak@student.lnu.se>
2025-09-26 10:10:46 +0200

    Add JSDoc for CodeBlockConverter

--- src/converters/CodeBlockConverter.js
+++ src/converters/CodeBlockConverter.js
@@ -1,25 +1,43 @@
 import { AbstractConverter } from './AbstractConverter'
 
+/**
+ * Converts markdown code blocks to HTML pre and code elements.
+ *
+ * This converter processes markdown code blocks (text enclosed in triple backticks)
+ * and converts them to HTML <pre><code> elements. It works by splitting text into
+ * blocks and identifying those that start with triple backticks.
+ *
+ * @extends AbstractConverter
+ */
 export class CodeBlockConverter extends AbstractConverter {
+  /**
+   * Converts markdown code blocks to HTML pre and code elements.
+   *
+   * Processes text blocks separated by double newlines, identifying code blocks
+   * (text starting and ending with triple backticks) and converting them to
+   * HTML <pre><code> elements. Non-code blocks are left unchanged.
+   *
+   * @param {string} text - The input text containing markdown code block syntax
+   * @returns {string} -  Text with markdown code blocks converted to HTML elements
+   * @override
+   */
+  convert(text) {
+    const textBlocks = text.split('\n\n')
 
-    convert(text) {
-        const textBlocks = text.split('\n\n')
+    const taggedBlocks = textBlocks.map((block) => {
+      if (!this.#isCodeBlock(block)) {
+        return block
+      }
 
-        const taggedBlocks = textBlocks.map(block => {
-            if (!this.#isCodeBlock(block)) {
-                return block
-            }
+      const trimmedBlock = block.substring(3, block.length - 3)
 
-            const trimmedBlock = block.substring(3, block.length - 3)
+      return `<pre><code>${trimmedBlock}</code></pre>`
+    })
 
-            return `<pre><code>${trimmedBlock}</code></pre>`
-        })
+    return taggedBlocks.join('\n\n')
+  }
 
-        return taggedBlocks.join('\n\n')
-    }
-
-    #isCodeBlock(block) {
-        return block.startsWith('```')
-    }
-
+  #isCodeBlock(block) {
+    return block.startsWith('```')
+  }
 }
