21640fec03ee91500455d310b649e92d25d29cec
Andreas Eriksson <ae225ak@student.lnu.se>
2025-09-26 09:30:08 +0200

    Work on CodeBlockConverter

--- src/converters/CodeBlockConverter.js
+++ src/converters/CodeBlockConverter.js
@@ -2,6 +2,27 @@ import { AbstractConverter } from './AbstractConverter'
 
 export class CodeBlockConverter extends AbstractConverter {
 
-    convert(text) {}
+    convert(text) {
+        const textBlocks = text.split('\n\n')
+
+        const taggedBlocks = textBlocks.map(block => {
+            if (!this.#isCodeBlock(block)) {
+                return block
+            }
+
+            console.log('block:' + block)
+            
+            console.log(block.substring(3, block.length - 3))
+
+
+            return `${block}`
+        })
+
+        return taggedBlocks.join('\n\n')
+    }
+
+    #isCodeBlock(block) {
+        return block.startsWith('```')
+    }
 
 }
