034839a9abb35a9ce71bdb165c6734971ecfc820
Andreas Eriksson <ae225ak@student.lnu.se>
2025-09-26 10:06:34 +0200

    Add more tests to CodeBlockConverter

--- tests/CodeBlockConverter.test.js
+++ tests/CodeBlockConverter.test.js
@@ -14,4 +14,27 @@ describe('CodeBlockConverter', () => {
 
     expect(converter.convert(input)).toBe(expected)
   })
+
+  it('should handle empty code block', () => {
+    const input = '```\n```'
+    const expected = '<pre><code>\n</code></pre>'
+
+    expect(converter.convert(input)).toBe(expected)
+  })
+
+  it('should convert multiple code blocks in same text', () => {
+    const input = '```\ncode1\n```\n\nSome text\n\n```\ncode2\n```'
+    const expected =
+      '<pre><code>\ncode1\n</code></pre>\n\nSome text\n\n<pre><code>\ncode2\n</code></pre>'
+
+    expect(converter.convert(input)).toBe(expected)
+  })
+
+  it('should leave non-code-block text unchanged', () => {
+    const input = 'Regular text\n\n```\ncode here\n```\n\nMore text'
+    const expected =
+      'Regular text\n\n<pre><code>\ncode here\n</code></pre>\n\nMore text'
+
+    expect(converter.convert(input)).toBe(expected)
+  })
 })
